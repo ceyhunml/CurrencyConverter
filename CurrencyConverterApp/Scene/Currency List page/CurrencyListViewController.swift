75ced2411436d65eaf385d051cccd30d88543eee
ceyhunml <ceyhunmmli@gmail.com>
2026-02-10 11:25:20 +0400

    Add search bar and filtering to currency list
    Introduce a UISearchBar and filtering support to CurrencyListViewController. Adds a filteredCurrencies array, a searchBar property, and a textChanged handler that filters the currencies list (case-insensitive) and reloads the table. Initializes filteredCurrencies in viewDidLoad and updates table data source and selection logic to use filteredCurrencies. Also updates layout constraints to place the search bar above the table view.

--- CurrencyConverterApp/Scene/Currency List page/CurrencyListViewController.swift
+++ CurrencyConverterApp/Scene/Currency List page/CurrencyListViewController.swift
@@ -39,6 +39,8 @@ final class CurrencyListViewController: UIViewController {
         "ZAR", "ZMW", "ZWL"
     ]
     
+    var filteredCurrencies = [String]()
+    
     var onSelect: ((String) -> Void)?
     
     private lazy var tableView: UITableView = {
@@ -49,20 +51,46 @@ final class CurrencyListViewController: UIViewController {
         return table
     }()
     
+    private lazy var searchBar: UISearchBar = {
+        let b = UISearchBar()
+        b.placeholder = "Search currency"
+        b.translatesAutoresizingMaskIntoConstraints = false
+        return b
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        filteredCurrencies = currencies
         configureUI()
     }
     
     func configureUI() {
         view.addSubview(tableView)
+        view.addSubview(searchBar)
+        
+        searchBar.searchTextField.addTarget(self, action: #selector(textChanged), for: .editingChanged)
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 2),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    @objc private func textChanged() {
+        let query = searchBar.searchTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        
+        if query.isEmpty {
+            filteredCurrencies = currencies
+        } else {
+            filteredCurrencies = currencies.filter { $0.localizedCaseInsensitiveContains(query) }
+        }
+        tableView.reloadData()
     }
 }
 
@@ -70,17 +98,17 @@ final class CurrencyListViewController: UIViewController {
 extension CurrencyListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        currencies.count
+        filteredCurrencies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = currencies[indexPath.row]
+        cell.textLabel?.text = filteredCurrencies[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selected = currencies[indexPath.row]
+        let selected = filteredCurrencies[indexPath.row]
         onSelect?(selected)
         dismiss(animated: true)
     }
